6fb4101db8488bfd4a59389341e3a30895cda8ed
Eric Martz <ericmartz@gmail.com>
2016-01-17 20:11:02 -0500

    Updated tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -4,127 +4,64 @@
  * all of the tests that will be run against your application.
  */
 
-/* NOTE TO REVIEWER:
- * So what I am finding is that there are so many different ways
- * to test values and to ensure you're getting what you want.
- * if you have any other ideas while running through the tests
- * I wrote I would love to hear them.
- */
 
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    /* This is the test suite for the RSS Feeds */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        /* This tests that the allFeeds array is defined and not empty */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO[Complete]: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         /* For modularity, I separated the tests that the URL is 
-          * defined and that the URL is not empty.
-          * Mostly, It looks like making tests unique and testing
-          * one item is a good idea, even if it has multiple expectations.
-          */
+        /* Testing that the RSS Feeds have a URL */
         it('has a URL', function(){
             allFeeds.forEach(function(feed){
                 expect(feed.url).toBeDefined();
             });
         });
 
+        /* Testing that the URL in each RSS Feed has something in it */
         it('URL is not empty', function(){
             allFeeds.forEach(function(feed){
-                /* I wanted a way to ensure the URL was not NULL, but I also
-                 * wanted to ensure the URL has a value. I thought testing that
-                 * the URL contains 'http://' would be a good idea.
-                 */
-                expect(feed.url).not.toBeNull();
-                expect(feed.url).toContain("http://");
+                expect(feed.url.length).toBeGreaterThan(0);
             });
         });
 
-        /* TODO[Complete]: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
-        it('has a name', function(){
+        /* Tests that each RSS has a name and is not empty */
+        it('has a name that is not empty', function(){
             allFeeds.forEach(function(feed){
-                expect(feed.name).toBeDefined();
-            });
-        });
-
-        it('name is not empty', function(){
-            allFeeds.forEach(function(feed){
-                /* I wanted a way to ensure that feed.name was not NULL,
-                 * but I also want to ensure it has a value. I originally
-                 * used not.toBeNull(), but then added toBeTruthy() to ensure
-                 * that the feed.name has a value. Since NULL evaluates
-                 * to FALSE, I took out not.toBeNull().
-                 * It is interesting looking through the different matchers
-                 * and realizing how they can be used in interesting ways.
-                 */ 
-                // expect(feed.name).not.toBeNull();
+                /* toBeTruthy will test that the name is defined and has a value. */
                 expect(feed.name).toBeTruthy();
             });
         });
     });
 
-    /* TODO[Complete]: Write a new test suite named "The menu" */
+    /* Test suite for The Menu */
     describe('The Menu', function() {
-        /* TODO[Complete]: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        /* Test checks that the menu is hidden by default */
         it('is hidden by default', function(){
-            /* Performing this test two ways,
-             * although they test the same thing.
-             * First we just get all the html and
-             * see if menu-hidden is in there.
-             * Then I am also utilizing jasmine-jQuery to 
-             * get see if the body element has a certain class.
-             * I like the 2nd way better, but I wasn't sure about
-             * using jasmine-jQuery as part of this project. 
+            /* I am utilizing jasmine-jQuery to 
+             * see if the body element has a certain class.
              */
-            $html = $('html').html();
-            $body = $('body');
-
-            expect($html).toContain('menu-hidden');
+            var $body = $('body');
             expect($body).toHaveClass('menu-hidden');
         });
 
-         /* TODO[Complete
-         ]: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+         /* Test checks that the menu is changes visibility when clicked. */
         it('changes visibility when clicked', function(){
 
-            /* Gots to store some DOM elements.
+            /* Got to store some DOM elements.
              * At first, I thought I would need to get the body DOM element
              * after doing the clicks, but tried moving it up here and it 
              * still works.
              */
-            $body = $('body');
-            $menu = $('.menu-icon-link');
+            var $body = $('body');
+            var $menu = $('.menu-icon-link');
 
             /* First we test that when the menu is clicked, it makes the
              * menu visible.
@@ -139,83 +76,76 @@ $(function() {
             expect($body).toHaveClass('menu-hidden');
         });
     });
-    /* TODO[Complete]: Write a new test suite named "Initial Entries" */
+
+    /* Testing Initial Entries */
     describe('Initial Entries', function(){
-        /* TODO[Complete]: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+
+        /* Loading the initial entries. */
         beforeEach(function(done){
-            loadFeed(0, function(){
-                done();
-            });
+            loadFeed(0, done);
         });
 
-        it('contains an entry', function(done){
-            /* I feel like this is a gimmicky way to do this, but after looking
-             * at what we learned from the TDD course, and looking at what we are
-             * testing for, it seems to work.  I also took out the class entry, and
-             * the class entry-link from the template and the test failed.
+        /* Testing that the initial entry contains an entry */
+        it('contains an entry', function(){
+            /* Initially, I had this test working by seeing if entry was in
+             * the HTML of the feed class.  However, it was pointed out that
+             * this could cause a false passing expectation.
+             * After my tests were passing, I had looked at some forums testing
+             * that the feed entry has more than one item in it.
+             * But for what I am submitting, I got information here, which is also
+             * documented in the README
+             * https://discussions.udacity.com/t/async-tests-beforeeach-how-does-it-really-works/41386
              */
-            $feed = $('.feed').html();
-            $article = $('article');
-            expect($feed).toContain('entry');
-            /* Added this expectation after initially writing the test just to 
-             * further test.
-             */
-            expect($article).toHaveClass('entry');
-            /* After writing the tests and doing some more studying, I found
-             * the code in the next line.
-             * expect($('.feed .entry').length).toBeGreaterThan(0);
-             * I suspect this is more in line with what was wanted, since the
-             * specification says at least one entry.  But what I initially
-             * wrote were the two tests above, and I figure that if it has one
-             * entry then it has at least one entry. Just wanted to put my 
-             * reasoning in here. I think both work, but I wanted to 
-             * acknowledge that a found another way that was very clever.
-             */
-            done();
+            var $feed = $('.feed .entry');
+            
+            expect($feed.length).toBeGreaterThan(0);
         });
     });
-    /* TODO[Complete]: Write a new test suite named "New Feed Selection" */
-    describe('New Feed Selection', function(){
-        /* TODO[Complete]: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
 
+    /* Testing when a new feed is selected.  */
+    describe('New Feed Selection', function(){
         /* Documented this is in the README file, but I wanted to also put this
          * here in the file. Initially, I thought that I would somehow use a spy
          * to track changes being made to the feed, but it did not look like that
          * would work after reading the documentation.  After that I had trouble
          * wrapping my head around how to test this and checked the forums.
          * Got some good help from the following posts:
+         * https://discussions.udacity.com/t/p6-new-feed-selection-test-question-problem/15562/11
          * https://discussions.udacity.com/t/last-test-in-the-project/15118/2
          * https://discussions.udacity.com/t/project-6-final-test-issue/17187
+         * https://discussions.udacity.com/t/async-tests-beforeeach-how-does-it-really-works/41386
          */
 
-        /* First store the feed HTML in a variable */
-        var feedContentPreChange = $('.feed').html();
+        /* Going to try and show that I know what is going on here.
+         * First of all, I am declaring the variables I want tp compare here.
+         */
+        var feedContentPreChange;
+        var feedContentPostChange;
         
-        /* Run beforeEach to load a feed other than 0, since that is what loads
-         * automatically
+        /* Run beforeEach to load a feed 0, since that is what loads
+         * automatically.
          */
         beforeEach(function(done){
-            loadFeed(1, function(){
+            loadFeed(0, function(){
+                feedContentPreChange = $('.feed').html();
                 done();
             });
         });
 
+        /* Once the done() method is called, the it block is allowed to run. */
         it('content changes on selection', function(done){
-            /* Storing the new feed HTML in a variable */
-            var feedContentPostChange = $('.feed').html();
-
-            /* Checking if the new feed and original feed HTML is different */
-            expect(feedContentPostChange).not.toEqual(feedContentPreChange);
-
-            done();
+            /* Now, I need to call loadFeed again to get a new feed.
+             * I am sending in a callback function, and in the callback
+             * I am getting the HTML of the feed.
+             * My understanding, is basically I am getting the HTML of the feed
+             * on line 73 of app.js and then I run my expectation.
+             * Then I call done(), which allows Jasmine to know I am done.
+             */
+            loadFeed(1, function(){
+                feedContentPostChange = $('.feed').html();
+                expect(feedContentPostChange).not.toEqual(feedContentPreChange);
+                done();
+            });
         });
     });
 
@@ -229,32 +159,23 @@ $(function() {
          * There is a place for a contentSnippet, so I figured we should
          * test for that.
          */
-        xit('should show content snippet', function(){
+        xit('each feed should have a contentSnippet defined', function(){
             allFeeds.forEach(function(feed){
-                /* First we check to make sure the contentSnippet exists. */
-                expect(feed.contentSnippet).toBeDefined();
-
-                /* Second we make sure that there is a value in there. */
                 expect(feed.contentSnippet).toBeTruthy();
             });
         });
 
-        /* One functionality I thought would be cool is if the Feed Reader
-         * loaded the entry content as an AJAX request.  So instead leaving the
-         * Feed Reader when a user clicks, you could have the article show up as an
-         * accordian type functionality.
-         */ 
-        xit('should load content as an AJAX request', function(){
-            /* So, I am just labeling this as a test stub. */
-            /* I fooled around with it for awhile, but wasn't sure how to test
-             * that we got content and then added it to our application.
-             * I saw in the AJAX request results there is a summary and a description
-             * but not anything more.  So it may be that without an API to get the content
-             * of the clicked entry, this idea is not currently possible.
-             * Also, there may be copyright issues. Not that I want to steal content,
-             * just make it easier for the reader to click items without leaving
-             * the Feed Reader.
-             */
+        /* Assumption in this test is that the contentSnippet is loaded
+         * during the loadFeed function.
+         */
+        beforeEach(function(done){
+            loadFeed(0, done);
+        });  
+
+        xit('initial feed should show a contentSnippet', function(){
+            /* Assuming the contentSnippet will have the class contentSnippet */
+            var $feed = $('.feed .contentSnippet');
+            expect($feed.length).toBeGreaterThan(0);    
         });
     });
 }());
